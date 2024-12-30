45aca534d4c1859fe15790a27617c5091404958f
Heribert Schütz <hs@webxcerpt.com>
2024-12-30 21:20:00 +0100

    added hexagonal prisms
    2 versions with "technical" differences
    
    ...and built.

--- src/examples.ts
+++ src/examples.ts
@@ -1,6 +1,8 @@
 import Thurston from "./examples/Thurston.json5?raw";
 import icosahedron from "./examples/icosahedron.json5?raw";
 import wrinkledIcosahedron from "./examples/wrinkled-icosahedron.json5?raw";
+import hexagonalPrism1 from './examples/hexagonal-prism.json5?raw';
+import hexagonalPrism2 from './examples/hexagonal-prism-2.json5?raw';
 import cube from "./examples/cube.json5?raw";
 import octahedron from "./examples/octahedron.json5?raw";
 import tetrahedron from "./examples/tetrahedron.json5?raw";
@@ -11,6 +13,8 @@ export const examples: {name: string, value: string}[] = [
   {name: "Thurston", value: Thurston},
   {name: "regular icosahedron", value: icosahedron},
   {name: "wrinkled icosahedron", value: wrinkledIcosahedron},
+  {name: "hexagonal prism #1", value: hexagonalPrism1},
+  {name: "hexagonal prism #2", value: hexagonalPrism2},
   {name: "cube", value: cube},
   {name: "regular octahedron", value: octahedron},
   {name: "regular tetrahedron", value: tetrahedron},
